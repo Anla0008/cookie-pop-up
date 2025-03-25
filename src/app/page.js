4cdd546a92153bd65ff6b9c950955d13f25dc1d6
Anarikke <anla0008@stud.kea.dk>
2025-03-25 11:43:20 +0100

    like knap

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Likebutton from "@/components/Likebutton";
 
 export default function Home() {
-  return <div>Dav Dav</div>;
+  return (
+    <div>
+      <Likebutton label="Like" />
+      <Likebutton label="Dislike" />
+    </div>
+  );
 }
